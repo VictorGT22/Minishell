9700261cf1ded0549b5dd7e2d0ea7e535fde873a
faltrum <70612135+faltrum@users.noreply.github.com>
2024-04-27 17:14:30 +0200

    Makefile y otros cambios

--- src/func_ptr/func_ptr2.d
+++
@@ -1,9 +0,0 @@
-src/func_ptr/func_ptr2.o: src/func_ptr/func_ptr2.c \
-  src/func_ptr/../../includes/minishell.h readline/readline.h \
-  readline/rlstdc.h readline/rltypedefs.h readline/keymaps.h \
-  readline/chardefs.h readline/tilde.h readline/history.h \
-  src/func_ptr/../../includes/../readline/readline.h \
-  src/func_ptr/../../includes/../readline/history.h \
-  src/func_ptr/../../includes/../libft/libft.h \
-  src/func_ptr/../../includes/structs.h \
-  src/func_ptr/../../includes/definitions.h
